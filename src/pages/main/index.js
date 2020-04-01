844196587279d4ff402350ca38d1f9df54d7c101
Vakhterova <59884901+Vakhterova@users.noreply.github.com>
2020-04-01 12:51:19 +0400

    catalog,toys_error

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,25 +1,25 @@
-var button = document.querySelector(".poster-link-js");
-var modalForm = document.querySelector(".modal-form-js");
-var buttonClose = document.querySelector(".modal-form-x-js");
-var input = document.querySelector(".form-input-js");
+var button = document.querySelector(".poster-link_js");
+var modalForm = document.querySelector(".modal-form_js");
+var buttonClose = document.querySelector(".modal-form__x_js");
+var input = document.querySelector(".form-input_js");
 
 
 //открытие
 button.addEventListener("click",function() {
-  modalForm.classList.remove("modal-form-none") 
+  modalForm.classList.remove("modal-form_none") 
   input.focus ();
 });
 
 //Закрытие
 buttonClose.addEventListener("click",function() {
-  modalForm.classList.add("modal-form-none") 
+  modalForm.classList.add("modal-form_none") 
   button.focus ();
 });
 
 //Закрытие escape
 window.addEventListener ("keydown", function (event) {
-  if (event.code == "Escape" && !modalForm.classList.contains ("modal-form-none")) { 
-  modalForm.classList.add("modal-form-none");
+  if (event.code == "Escape" && !modalForm.classList.contains ("modal-form_none")) { 
+  modalForm.classList.add("modal-form_none");
   button.focus ()
 } 
 });
